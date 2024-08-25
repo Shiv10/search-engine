9092c4624c4c6e179a6e051cdd7f2c311f80a8a4
Shiv10 <shivanshsharma2012@gmail.com>
2024-08-25 19:43:35 +0530

    feat: init setup

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"search-engine/routes"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	env := godotenv.Load()
+
+	if env != nil {
+		panic("cannot find environment variabled")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = ":4000"
+	} else {
+		port = ":" + port
+	}
+
+	app := fiber.New(fiber.Config{
+		IdleTimeout: 5 * time.Second,
+	})
+
+	app.Use(compress.New())
+
+	routes.SetRoutes(app)
+
+	// Start server and listen for shutdown
+	go func() {
+		if err := app.Listen(port); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c // block the main thread
+	app.Shutdown()
+	fmt.Println("Shutting down server")
+}
